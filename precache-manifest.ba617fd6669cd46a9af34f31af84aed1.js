0c058302994c87e9bc2f5af379f2ead81a0a90d6
Home Dingo <aphanit@protonmail.com>
2021-12-24 21:24:21 -0500

    Updates

---
+++ precache-manifest.ba617fd6669cd46a9af34f31af84aed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d9bd1df08c145583d94",
+    "url": "/react-iv/static/css/main.d588cd2b.chunk.css"
+  },
+  {
+    "revision": "7d9bd1df08c145583d94",
+    "url": "/react-iv/static/js/main.7d9bd1df.chunk.js"
+  },
+  {
+    "revision": "6d5f532687ceebe82fd8",
+    "url": "/react-iv/static/js/1.6d5f5326.chunk.js"
+  },
+  {
+    "revision": "d5b8677cf9ea1ad7ac1f",
+    "url": "/react-iv/static/js/runtime~main.d5b8677c.js"
+  },
+  {
+    "revision": "4d0030b96620027addc34a827fcb9226",
+    "url": "/react-iv/static/media/happy-nurse.4d0030b9.jpg"
+  },
+  {
+    "revision": "97f80c63891f511d4ff5546d5c40d74d",
+    "url": "/react-iv/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-iv/precache-manifest.0ef659913c0e33e33d0e272645c289cc.js"
+  "/react-iv/precache-manifest.ba617fd6669cd46a9af34f31af84aed1.js"
 );
 
 workbox.clientsClaim();
